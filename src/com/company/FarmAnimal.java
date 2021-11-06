94692afdaf3bcc054907f13047ee56dcd65780b7
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-06 12:59:41 +0100

    Added new classes.

---
+++ src/com/company/FarmAnimal.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class FarmAnimal extends Animal{
+    public FarmAnimal(String species) {
+        super(species);
+    }
+}
---
+++ src/com/company/Pet.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Pet extends Animal{
+    public Pet(String species) {
+        super(species);
+    }
+}
