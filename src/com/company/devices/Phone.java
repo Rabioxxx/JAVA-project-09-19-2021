68fcc63d2b51ee6b8f2978528414eedb9dacea78
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-09 19:53:55 +0100

    Zadanie 10.2

--- src/com/company/devices/Phone.java
+++ src/com/company/devices/Phone.java
@@ -5,11 +5,23 @@ import com.company.creatures.Human;
 public class Phone extends Device {
     Double screenSize;
     String os;
+    static final String defaultServerURL = "https://www.downloadalltheappshere.com";
+    static final String defaultProtocol = "Protocol0000";
+    static final String defaultVersionName = "v1.0";
 
     public Phone(String model, String producer, Double value, Integer yearOfProduction, Double screenSize, String os) {
         super(model, producer, value, yearOfProduction);
         this.screenSize = screenSize;
         this.os = os;
+    }
+
+    public void installAnApp(String appName) {
+    }
+
+    public void installAnApp(String appName, String version) {
+    }
+
+    public void installAnApp(String appName, String version, String server) {
     }
 
     public void sell(Human seller, Human buyer, Double price) {
