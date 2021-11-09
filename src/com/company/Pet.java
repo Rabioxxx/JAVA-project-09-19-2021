3c0f94368897366bbe6088a1ed68ee401c1b0ad3
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-09 02:30:52 +0100

    Adjustments.

--- src/com/company/Pet.java
+++ src/com/company/Pet.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Pet extends Animal{
-    public Pet(String species) {
-        super(species);
+    public Pet(String species, Double weight, Integer age) {
+        super(species, weight, age);
     }
 }
