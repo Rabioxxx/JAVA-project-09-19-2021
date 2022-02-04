9369286ffe5a366a39f61d5f0e8ab92eb06dfec8
Szymon Jasiński <szg.jasinski@gmail.com>
2022-02-04 18:55:24 +0100

    Zadanie 13, removed wrong to add new one.

--- src/com/company/devices/Applications.java
+++ src/com/company/devices/Applications.java
@@ -1,8 +1,22 @@
 package com.company.devices;
 
 public class Applications {
-    String appName;
-    Applications(String appName){
-        this.appName = appName;
+    String name;
+    String version = "v1.0.0";
+    Double price = 0.0;
+
+    Applications(String name){
+        this.name = name;
+    }
+
+    public Applications(String name, String version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    public Applications(String name, String version, Double price) {
+        this.name = name;
+        this.version = version;
+        this.price = price;
     }
 }
--- src/com/company/devices/Phone.java
+++ src/com/company/devices/Phone.java
@@ -9,42 +9,12 @@ import java.util.List;
 public class Phone extends Device {
     Double screenSize;
     String os;
-    static final String defaultServerURL = "downloadalltheappshere.com";
-    static final Integer defaultProtocol = 443;
-    static final String defaultVersionName = "v1.0";
+    Applications[] apps;
 
     public Phone(String model, String producer, Double value, Integer yearOfProduction, Double screenSize, String os) {
         super(model, producer, value, yearOfProduction);
         this.screenSize = screenSize;
         this.os = os;
-    }
-
-    public void installAnApp(String appName) {
-        installAnApp(appName, defaultVersionName);
-    }
-
-    public void installAnApp(String appName, String version) {
-        installAnApp(appName, version, defaultServerURL);
-    }
-
-    public void installAnApp(String appName, String version, String server) {
-
-        try {
-            URL url = new URL("https", server, defaultProtocol, appName + "-" + version);
-            this.installAnApp(url);
-        } catch (MalformedURLException e){
-            e.printStackTrace();
-        }
-    }
-
-    public void installAnApp(List<String> appList) {
-        for (String appName : appList) {
-            this.installAnApp(appName);
-        }
-    }
-
-    public void installAnApp(URL url) {
-        System.out.println("Installed correctly from " + url + ".");
     }
 
     public void sell(Human seller, Human buyer, Double price) {
